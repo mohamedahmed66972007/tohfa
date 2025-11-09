2afd50f0db9574d7ead40518f01dea75c891b815
mohamedahmed66972007 <123965732+mohamedahmed66972007@users.noreply.github.com>
2025-11-10 02:01:11 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -65,7 +64,7 @@ function App() {
     timerMinutes: number;
   }) => {
     const { name, questions, randomizeQuestions, randomizeOptions, enableTimer, timerMinutes } = data;
-    
+
     if (editingContestantId) {
       // تحديث contestant موجود
       const updatedContestants = contestants.map((c) => {
