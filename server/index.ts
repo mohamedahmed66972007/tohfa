8be59dd12b05354d0ee9ac4d7e7ee5fcaa5a6089
mohamedahmed66972007 <123965732+mohamedahmed66972007@users.noreply.github.com>
2025-11-10 01:54:25 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -48,8 +48,6 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  const server = registerRoutes(app);
-
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
@@ -57,6 +55,8 @@ app.use((req, res, next) => {
     res.status(status).json({ message });
     throw err;
   });
+
+  const server = await registerRoutes(app);
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
